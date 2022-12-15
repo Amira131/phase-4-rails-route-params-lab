0af6c6823167e508773d72f2002d65a10ab81d40
Amira de Moya <demoyaamira@gmail.com>
2022-12-15 09:40:04 -0500

    commit tree 925d30e8538d1bd7b11c02f3496a896768cc9ef6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,18 @@
 class StudentsController < ApplicationController
 
   def index
+    
     students = Student.all
-    render json: students
+    render json: students, only: [:first_name, :last_name]
   end
+
+  def show
+  
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+
+
 
 end
